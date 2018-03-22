810d88378bf65f364abf026a2a30a965ed5fa855
yichung <ql3895619@gmail.com>
2018-03-22 17:17:01 +0800

    bugfix

--- mergeSort/mergeSort.cpp
+++ mergeSort/mergeSort.cpp
@@ -2,13 +2,13 @@
 #include <math.h>
 using namespace std;
 
-void Merge(int* array, int p, int q, int r);
-void MergeiSort(int* array, int p, int r);
+void Merge(int* , int , int , int);
+void MergeSort(int* , int , int);
 
 int main(){
   int len = 6;
-  int* array = {3, 4, 8, 1, 68, 1};
-  MergeSort(array, 0, len - 1);
+  int array[] = {3, 4, 8, 1, 68, 1};
+  MergeSort(array, 0, len-1);
   for (int i = 0; i < len; i++){
     cout << array[i];
   }
@@ -16,7 +16,7 @@ int main(){
 
 void MergeiSort(int* array, int p, int r){
   if(p<r){
-    q = floor((p + t) / 2);
+    int q = floor((p + r) / 2);
     MergeSort(array, p, q);
     MergeSort(array, q+1, r);
     Merge(array, p, q, r);
@@ -28,10 +28,10 @@ void Merge(int* array, int p, int q, int r){
   int n2 = r - q;
   int L[n1 + 1];
   int R[n2 + 1];
-  for(int i = 0; i < n1; i++) L[i]=A[p + i];
-  for(int i = 0; i < n2; i++) R[i]=A[q + j + 1];
+  for(int i = 0; i < n1; i++) L[i]=array[p + i];
+  for(int i = 0; i < n2; i++) R[i]=array[q + 1 + 1];
   L[n1] = 0x7fffffff;
-  R[n3] = 0x7fffffff;
+  R[n2] = 0x7fffffff;
   int I = 0;
   int J = 0;
   for(int i = p; i <= r ; i++ ){
