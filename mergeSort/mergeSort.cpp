2e813074d9b65ad98748eec840eaa6209a449038
yichung <ql3895619@gmail.com>
2018-03-22 22:32:41 +0800

    add read and write file

--- mergeSort/mergeSort.cpp
+++ mergeSort/mergeSort.cpp
@@ -1,17 +1,48 @@
 #include<iostream>
+#include<fstream>
+#include<sstream>
+#include <string>
 #include <math.h>
 using namespace std;
 
 void Merge(int* , int , int , int);
 void MergeSort(int* , int , int);
+int str2int(string);
+string int2str(int num);
 
 int main(){
-  int len = 6;
-  int array[] = {3, 4, 8, 1, 68, 1};
-  MergeSort(array, 0, len-1);
-  for (int i = 0; i < len; i++){
-    cout << array[i];
+  ifstream fin;
+  fin.open("test.txt");
+  string line;
+  getline(fin,line);
+  int len = str2int(line);
+  int array[len];
+  int i = 0;
+  while(getline(fin,line)){
+    array[i] = str2int(line);
+    i++;
   }
+  MergeSort(array, 0, len-1);
+  ofstream fout;
+  fout.open ("output.txt");
+  fout << int2str(len) << endl;
+  for(int i = 0; i < len; i++) fout << int2str(array[i]) << endl;
+}
+
+int str2int(string numStr){
+  stringstream ss;
+  int num;
+  ss << numStr;
+  ss >> num;
+  return num;
+}
+
+string int2str(int num){
+  stringstream ss;
+  string numStr;
+  ss << num;
+  ss >>  numStr; 
+  return numStr;
 }
 
 void MergeSort(int* array, int p, int r){
